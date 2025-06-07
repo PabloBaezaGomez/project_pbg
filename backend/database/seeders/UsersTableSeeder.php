e74b4ad4cf38c1a0bd5616b36215367b658fe7cd
PabloBaezaGomez <pablo.bae.go@gmail.com>
2025-06-07 15:28:51 +0200

    Admin instead of random name

--- backend/database/seeders/UsersTableSeeder.php
+++ backend/database/seeders/UsersTableSeeder.php
@@ -10,7 +10,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['user_name' => 'Alice', 'user_email' => 'alice@example.com', 'user_password' => bcrypt('password'), 'user_type' => 'admin'],
+            ['user_name' => 'Admin', 'user_email' => 'admin@example.com', 'user_password' => bcrypt('password'), 'user_type' => 'admin'],
             ['user_name' => 'Bob', 'user_email' => 'bob@example.com', 'user_password' => bcrypt('password'), 'user_type' => 'user'],
             ['user_name' => 'Charlie', 'user_email' => 'charlie@example.com', 'user_password' => bcrypt('password'), 'user_type' => 'user'],
             ['user_name' => 'Diana', 'user_email' => 'diana@example.com', 'user_password' => bcrypt('password'), 'user_type' => 'user']
